3b62fc8fc30a4acbac96e3de1aca4cfd7338fcc7
danielfurman03369 <danielfurman03@gmail.com>
2026-01-28 21:32:17 +0200

    for range

---
+++ for range.py
@@ -0,0 +1,15 @@
+
+'''
+i:int = 0
+
+while True:
+    print(i, end=" ")
+    i += 1
+
+    if i > 10:
+        break
+'''
+
+
+for x in range(1, 10 + 1):  # or (1, 11) one more than you need
+    print(x, end=" ")
